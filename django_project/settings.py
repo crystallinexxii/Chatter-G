81a327af4be4c3fbd04d3d38699df4eec1e97de8
Joseph Chacko <112103666+maverik-io@users.noreply.github.com>
2024-03-31 12:12:47 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'NAbTErXiS80F2U5NgUNSwgnDb4U8xgLiOU3qu5GRAPs'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
